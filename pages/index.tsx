a0379995d0b6c760714768525db6b9d35d5fc3e7
Justin Singh <justinsingh2015@gmail.com>
2022-02-22 15:00:55 -0800

    Styling improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { VStack, Text, Center, Link } from '@chakra-ui/react'
+import { VStack, Text, Center, Link, Box } from '@chakra-ui/react'
 const emoji = require("emoji-dictionary");
 
 const Home: NextPage = () => {
   return (
-    <Center>
-      <VStack marginTop="30vh">
-        <Text fontSize={"4vw"}>
-          <Link color="purple.400" href="https://twitter.com/ByJustinSingh" isExternal>justin singh </Link>
-          is still figuring out what
-          <Link color="pink.400" href="https://twitter.com/web3toons" isExternal> web3toons </Link>
-          is
-        </Text>
-        <Text fontSize={"3vw"}>ty for your interest {emoji.getUnicode("two_hearts")}</Text>
-      </VStack>
-    </Center >
+    <Box backgroundColor="yellow.100" minW="100vw" minH="100vh">
+      <Center>
+        <VStack marginTop="20vh" maxW={["75vw"]}>
+          <Text fontSize={"7vw"}>
+            <Link color="purple.400" href="https://twitter.com/ByJustinSingh" isExternal>justin singh </Link>
+            is figuring out what
+            <Link color="pink.400" href="https://twitter.com/web3toons" isExternal> web3toons </Link>
+            is!
+          </Text>
+          <Text fontSize={"7vw"}>ty for your interest {emoji.getUnicode("two_hearts")}</Text>
+        </VStack>
+      </Center >
+    </Box>
   )
 }
 
