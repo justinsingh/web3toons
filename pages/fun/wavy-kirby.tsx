fe4f2bf575068f672e0c363409201e6df9d01005
Justin Singh <justinsingh2015@gmail.com>
2022-03-01 01:34:39 -0800

    Improve responsiveness

--- pages/fun/wavy-kirby.tsx
+++ pages/fun/wavy-kirby.tsx
@@ -4,12 +4,15 @@ import WavyKirby from '../../components/WavyKirby'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Box, VStack } from '@chakra-ui/react'
+import { useMediaQuery } from '@chakra-ui/react'
 
 const WavyKirbyPage: NextPage = () => {
+  const [isMobile] = useMediaQuery("(max-width: 30em)");
+
   return (
     <VStack>
         <Box width={["100vw"]} height={["100vh"]} backgroundColor="black">
-          <Canvas camera={{ fov: 10, position: [0, 0, 100] }} color="black">
+          <Canvas camera={{ fov: ( isMobile ? 25 : 10), position: [0, 0, 100] }} color="black">
             <Suspense fallback={null}>
               <OrbitControls />
               <pointLight position={[10, 10, 10]} />
