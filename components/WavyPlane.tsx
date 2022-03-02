ee7deebe42583f755cd5b81cf26154d7f9730645
Justin Singh <justinsingh2015@gmail.com>
2022-03-01 23:51:44 -0800

    Import WaveShaderMaterial

--- components/WavyPlane.tsx
+++ components/WavyPlane.tsx
@@ -1,60 +1,8 @@
 //@ts-nocheck
 import * as THREE from "three"
-import { useRef, Suspense } from "react"
-import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber"
-import { shaderMaterial } from "@react-three/drei"
-import glsl from "babel-plugin-glsl/macro"
-
-const WaveShaderMaterial = shaderMaterial(
-  // Uniform
-  {
-    uTime: 0,
-    uColor: new THREE.Color(0.0, 0.0, 0.0),
-    uTexture: new THREE.Texture(),
-  },
-  // Vertex Shader
-  glsl`
-    precision mediump float;
-
-    varying vec2 vUv;
-    varying float vWave;
-
-    uniform float uTime;
-
-    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
-
-    void main() {
-      vUv = uv;
-
-      vec3 pos = position;
-      float noiseFreq = 1.5;
-      float noiseAmp = 0.05; 
-      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
-      pos.z += snoise3(noisePos) * noiseAmp;
-      vWave = pos.z;
-
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-    }
-  `,
-  // Fragment Shader
-  glsl`
-    precision mediump float;
-
-    uniform vec3 uColor;
-    uniform float uTime;
-    uniform sampler2D uTexture;
-    
-    varying vec2 vUv;
-    varying float vWave;
-
-    void main() {
-      float wave = vWave * 0.1;
-      vec3 texture = texture2D(uTexture, vUv + wave).rgb;
-      gl_FragColor = vec4(texture, 1.0);
-    }
-  `
-)
-
+import { useRef } from "react"
+import { extend, useFrame, useLoader } from "@react-three/fiber"
+import WaveShaderMaterial from "./shaders/WaveShaderMaterial"
 extend({ WaveShaderMaterial });
 
 const WavyPlane = () => {
