0fd35c4d5d6f4ee923eb9b47586cb355c1dd46fe
Justin Singh <justinsingh2015@gmail.com>
2022-03-03 01:06:52 -0800

    Move Sphere to its own local component RedSphere

--- pages/fun/lulsasaurus.tsx
+++ pages/fun/lulsasaurus.tsx
@@ -1,12 +1,28 @@
+//@ts-nocheck
 import * as THREE from "three"
 import type { NextPage } from 'next'
-import { Suspense } from 'react'
+import { Suspense, useRef } from 'react'
 import WavyPlane from '../../components/WavyPlane'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, extend } from '@react-three/fiber'
 import { OrbitControls, Sphere } from '@react-three/drei'
 import { Box, VStack } from '@chakra-ui/react'
 import { ChromaticAberration, EffectComposer, Pixelation, Glitch, Noise } from '@react-three/postprocessing'
 import { useMediaQuery } from '@chakra-ui/react'
+import WaveShaderMaterial from "../../components/shaders/WaveShaderMaterial"
+extend({ WaveShaderMaterial })
+
+
+const RedSphere = () => {
+  const [isMobile] = useMediaQuery("(max-width: 30em)");
+  const ref = useRef();
+
+  return (
+    <Sphere scale={isMobile ? 2.0 : 3.0} position={[0, 0, 4]} ref={this}>
+      <meshPhongMaterial attach="material" color="red" />
+    </Sphere>
+
+  )
+}
 
 // Alternate name ideas: Love Sick, In Pursuit of Romance (prolly save this one for a bigger idea)
 const Heart: NextPage = () => {
@@ -20,20 +36,18 @@ const Heart: NextPage = () => {
             <OrbitControls />
             <pointLight position={[10, 10, 15]} />
             <WavyPlane />
-            <Sphere scale={isMobile ? 2.0 : 3.0} position={[0, 0, 4]}>
-              <meshPhongMaterial attach="material" color="red" />
-              <EffectComposer>
-                <Pixelation granularity={0} />
-                <Noise premultiply />
-                <Glitch
-                  delay={new THREE.Vector2(0.55, 1.7)} // min and max glitch delay
-                  duration={new THREE.Vector2(0.2, 0.5)} // min and max glitch duration
-                  strength={new THREE.Vector2(0.3, 1.0)} // min and max glitch strength
-                  active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
-                  ratio={0.55} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
-                />
-              </EffectComposer>
-            </Sphere>
+            <RedSphere />
+            <EffectComposer>
+              <Pixelation granularity={0} />
+              <Noise premultiply />
+              <Glitch
+                delay={new THREE.Vector2(0.55, 1.7)} // min and max glitch delay
+                duration={new THREE.Vector2(0.2, 0.5)} // min and max glitch duration
+                strength={new THREE.Vector2(0.3, 1.0)} // min and max glitch strength
+                active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
+                ratio={0.55} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+              />
+            </EffectComposer>
           </Suspense>
         </Canvas>
       </Box>
